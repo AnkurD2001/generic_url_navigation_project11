a528e16a5a3cb39b9c4d6b0f6d45c10e089c70bc
AnkurD2001 <ankur98amg@gmail.com>
2023-11-22 13:19:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def ankur(request):
+    return render(request,'ankur.html')
+
+def absa(request):
+    return render(request,'absa.html')
+
+def ankur2(request):
+    return render(request,'ankur2.html')
+
+def absa2(request):
+    return render(request,'absa2.html')
+
+
